08c2fdf5a22c012bb75e3d25de9acb5799ea0f23
ManuGS2 <manu.skate17@gmail.com>
2019-08-18 20:03:59 -0500

    envio y recepcion de mensajes

--- client.py
+++ client.py
@@ -1,12 +1,34 @@
 import socket
+from threading import Thread
 
 HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
 PORT = 65432        # Port to listen on (non-privileged ports are > 1023)
+BUFFER = 1024
+
+def msgFromServ(server):
+    while True:
+        msg = server.recv(BUFFER)
+        print("Server> "+str(msg))
+
+def msgToServ(client):
+    while True:
+        msg = input("Client> ")
+        client.send(bytes(msg,"utf-8"))
+        if msg == "exit":
+            break
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as cl:
     cl.connect((HOST, PORT))
-    cl.sendall(b'Prueba de mensaje')
+    thrCl = Thread(target=msgFromServ, args=(cl,))
+    thrSrv = Thread(target=msgToServ, args=(cl,))
+    thrCl.start()
+    thrSrv.start()
+    thrCl.join()
+    thrSrv.join()
 
+    while True:
+        msg = input()
+        
     data = cl.recv(1024)
 
 print('Received', repr(data))
--- server.py
+++ server.py
@@ -1,17 +1,41 @@
 import socket
+from threading import Thread
 
-HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
-PORT = 65432        # Port to listen on (non-privileged ports are > 1023)
+HOST = '127.0.0.1'  #IP standar para el localhost
+PORT = 65432        # Puerto válido para la conexion
+BUFFER = 1024       # Tamaño del buffer para los mensajes
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as serv:
-    serv.bind((HOST, PORT))       # Asocia el socket a la interfaz y la dirección
-    serv.listen()                 # Pone el puerto a escuchar
-    conn, addr = serv.accept()    # Habilita las conexiones /se retoner un Objeto socket y una tupla con la direccion del cliente (host, port)
-                                  # Accept devulve un socket (el del cliente) y su dir. de acuerdo a la AF
-    with conn:
-        print('Connected by', addr)
-        while True:
-            data = conn.recv(1024)
-            if not data:
-                break
-            conn.sendall(data)
+
+
+def connectToClient(serv):
+    client, addr = serv.accept()    # Habilita las conexiones /se retoner un Objeto socket y una tupla con la direccion del cliente (host, port)
+    print("Se ha conectado {}".format(addr))    # Accept devulve un socket (el del cliente) y su dir. de acuerdo a la AF
+    
+    thrCl = Thread(target=msgFromCl, args=(client,serv))
+    thrSrv = Thread(target=msgToCl, args=(client,))
+    thrCl.start()
+    thrSrv.start()
+    thrCl.join()
+    thrSrv.join()
+
+
+def msgFromCl(client, server):
+    while True:
+        msg = client.recv(BUFFER)
+        if msg == "exit":
+            break
+        else:
+            print("Client> " + str(msg))
+
+def msgToCl(client):
+    while True:
+        client.send(bytes(input("Server> "), "utf-8"))
+
+def main():
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as serv:
+        serv.bind((HOST, PORT))   # Asocia el puerto y la dirección a la iterfaz
+        serv.listen()             # Pone el puerto a escuchar
+        
+        print("Esperando una conexion...")
+        connectToClient(serv)
+main()
